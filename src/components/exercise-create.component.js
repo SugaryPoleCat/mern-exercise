c289f69f3fa78989eecee0e4dc787dcde70405d2
Sugar <sugarypolecat@gmail.com>
2021-10-14 11:19:47 +0200

    weoking on exercise create and started user create

--- src/components/exercise-create.component.js
+++ src/components/exercise-create.component.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default class ExerciseCreate extends Component {
 	// constructors
@@ -28,7 +30,18 @@ export default class ExerciseCreate extends Component {
 		}
 	}
 
-	onChangeUsername(e){
+	// this is react lifecycle method.
+	// different lifecycle mehtods will be called automatically at differnt points
+	// this is called right before anything on the page loads, this will run this code
+	componentDidMount() {
+		// hard code the users array for now.
+		this.setState({
+			users: ['test-user'],
+			username: 'test-user',
+		});
+	}
+
+	onChangeUsername(e) {
 		// changing a state, ou use this.setState();
 		// because he doesnt explain very well, e is basically the req.body of react.
 		// this just updates teh STATE for this one attribute in our state.
@@ -36,18 +49,18 @@ export default class ExerciseCreate extends Component {
 			username: e.target.value,
 		});
 	}
-	onChangeDescription(e){
+	onChangeDescription(e) {
 		this.setState({
 			description: e.target.value,
 		});
 	}
-	onChangeDuration(e){
+	onChangeDuration(e) {
 		this.setState({
 			duration: Number(e.target.value),
 		});
 	}
 	// because we are going to use a library taht creates a calender, so that you can pickdate, we are not using (e)
-	onChangeDate(date){
+	onChangeDate(date) {
 		// we want this to refer to this class. check up below the PROPRS again, the this.on things.
 		this.setState({
 			date: date,
@@ -55,7 +68,7 @@ export default class ExerciseCreate extends Component {
 	}
 
 	// when someone clicks the submit button
-	onSubmit(e){
+	onSubmit(e) {
 		// prevent default HTML form processing
 		e.preventDefault();
 
@@ -76,8 +89,49 @@ export default class ExerciseCreate extends Component {
 	// frontend
 	render() {
 		return (
+			// <div>
+			// 	<p>You are on the Exercise Create component!</p>
+			// </div>
+			// ill just make the same form he makes to follow along
 			<div>
-				<p>You are on the Exercise Create component!</p>
+				<h3>Create New Exercise</h3>
+				{/* call this.oNsubmit */}
+				<form onSubmit={this.onSubmit}>
+					<div className="form-group">
+						<label>Username: </label>
+						{/* this is a dropdown menu */}
+						{/* in here set teh state of username. and on change it will change.*/}
+						<select ref="userInput" required className="formControl" value={this.state.username} onChange={this.onChangeUsername}>
+							{
+								// this will create multiple options in hte dropdown box, based on our users array
+								// a normal map, assagining the key and value of the user to the option, so its usable.
+								this.state.users.map((user) => {
+									return <option key={user} value={user}>{user}</option>
+								})
+							}
+						</select>
+					</div>
+					<div className="form-group">
+						<label>Description: </label>
+						<input type="text" required className="form-control" value={this.state.description} onChange={this.onChangeDescription} />
+						{/* REmember to always set onChange={OnChangeMethod} in react, whenever you let users CHANGE anything on your webpage, as react will monitor the changes. */}
+					</div>
+					<div className="form-group">
+						<label>Duration in minutes: </label>
+						<input type="text" required className="form-control" value={this.state.duration} onChange={this.onChangeDuration} />
+					</div>
+					<div className="form-group">
+						<label>Date: </label>
+						<div>
+							<DatePicker
+								selected={this.state.date}
+								onChange={this.onChangeDate} />
+						</div>
+					</div>
+					<div className="form-group">
+						<input type="submit" value="Create Exercise" className="btn btn-primary" />
+					</div>
+				</form>
 			</div>
 		);
 	}
--- src/components/user-create.component.js
+++ src/components/user-create.component.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
 
 export default class UserCreate extends Component {
+	constructor(props) {
+		super(props);
+
+		this.onChangeUsername = this.onChangeUsername.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+
+		this.state = {
+			username: '',
+		}
+	}
+
+	componentDidMount() {
+		this.setState({
+			users: ['test-user'],
+			username: 'test-user',
+		});
+	}
+
+	onChangeUsername(e) {
+		this.setState({
+			username: e.target.value,
+		});
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+
+		const user = {
+			username: this.state.username,
+		};
+
+		console.log(user);
+
+		// window.location = '/';
+		this.setState({
+			username: '',
+		});
+		// make it blank so they can set a new user
+	}
+
 	render() {
 		return (
 			<div>
-				<p>You are on the User Create component!</p>
+				<h3>Create New Exercise</h3>
+				<form onSubmit={this.onSubmit}>
+					<div className="form-group">
+						<label>Username: </label>
+						<input />
+					</div>
+					<div className="form-group">
+						<input type="submit" value="Create Exercise" className="btn btn-primary" />
+					</div>
+				</form>
 			</div>
 		);
 	}
